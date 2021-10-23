1bbbf630a4e49a65a7a5082c4573c89501e8dca0
Michael Verges <maustinstar@gmail.com>
2021-10-22 21:19:55 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hypercut-native-foundation
+
+A description of this package.
---
+++ Tests/hypercut-native-foundationTests/hypercut_native_foundationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import hypercut_native_foundation
+
+final class hypercut_native_foundationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(hypercut_native_foundation().text, "Hello, World!")
+    }
+}
