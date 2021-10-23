2e2eb12b847e6559d2605b19e0d8048c88f145ef
Michael Verges <maustinstar@gmail.com>
2021-10-23 04:26:36 -0400

    public init

--- Sources/HypercutFoundation/TimecodeInterval.swift
+++ Sources/HypercutFoundation/TimecodeInterval.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 public struct TimecodeInterval: Codable {
+  public init(start: Double, end: Double) {
+    self.start = start
+    self.end = end
+  }
   public var start: Double
   public var end: Double
 }
