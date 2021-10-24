0e550ad4a85174b40879554af3760c6b6efcfcc4
Michael Verges <maustinstar@gmail.com>
2021-10-23 20:13:55 -0400

    space words

--- Sources/HypercutFoundation/Space.swift
+++ Sources/HypercutFoundation/Space.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct Space: Codable {
   public var start: TimeInterval
   public var end: TimeInterval
-  public var words: [Word]
+  public var words: [Word]?
   
   public var interval: TimecodeInterval {
     return .init(start: start, end: end)
